64c0084dd2e4d148e857ad9284b5504f6fc3b986
Josh Keim <joshuamkeim@gmail.com>
2022-05-27 13:31:09 -0400

    worked on layout of filter icon bar

--- client/src/components/FilterBar/index.js
+++ client/src/components/FilterBar/index.js
@@ -12,24 +12,46 @@ function FilterBar() {
                 </button>
             </div>
             <div className='icon-block'>
+            <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
                 <span className='filter-icon'><i class="fa-solid fa-money-bill-1"></i></span>
                 <span className='filter-name'>Expensive</span>
+            </button>
             </div>
             <div className='icon-block'>
+            <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
                 <span className='filter-icon'><i class="fa-solid fa-campground"></i></span>
                 <span className='filter-name'>Camping</span>
+            </button>
             </div>
             <div className='icon-block'>
+            <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
                 <span className='filter-icon'><i class="fa-solid fa-water"></i></span>
                 <span className='filter-name'>Lakefront</span>
+            </button>
             </div>
             <div className='icon-block'>
+            <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
                 <span className='filter-icon'><i class="fa-solid fa-building"></i></span>
                 <span className='filter-name'>City</span>
+            </button>
             </div>
             <div className='icon-block'>
+            <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
                 <span className='filter-icon'><i class="fa-solid fa-caravan"></i></span>
                 <span className='filter-name'>Camper</span>
+            </button>
+            </div>
+            <div className='icon-block'>
+                <button className='filter-icon-button' type= "button" aria-hidden="false" aria-pressed="true">
+                  <span className='filter-icon'><i class="fa-solid fa-chess-rook"></i></span>
+                  <span className='filter-name'>Castles</span>
+                </button>
+            </div>
+            <div className='filter-icon-block'>
+                <button className='filter-button' type= "button" aria-hidden="false" aria-pressed="true">
+                  <span><i class="fa-solid fa-filter"></i></span>
+                  <span className='filter-name'>Filter</span>
+                </button>
             </div>
         </div>
     </div>
