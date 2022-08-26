a79926fd0f201ec5eace610d8c92d511570fa301
Josh Keim <joshuamkeim@gmail.com>
2022-08-26 19:55:54 -0400

    changing room page

--- client/src/components/RoomDetail/index.js
+++ client/src/components/RoomDetail/index.js
@@ -65,30 +65,80 @@ function RoomDetail() {
 
 
   return (
-    <div>
-    <div className='room-block'>
-        <div className='room-info'>
-          <h1>{room?.location?.city}, {room?.location?.country}</h1>
-          <span>{avgRating} <i className="fa-solid fa-star fa-sm"></i></span>
-          <p>${room?.price}</p>
-      </div>
-        <img src={room?.file_name}
-        alt="" className='room-image'/>
-      <div>
-        <div>
-          <h4 className='description-tag'>{room?.description}</h4>
-        </div>
-      {(!showEditRoom && (sessionUser?.id === room?.owner_id)) && (
-            <button onClick={() => setShowEditRoom(true)} className="room-btn" >Edit</button>
-          )}
-          {(!showCreateReservation) && (
-            <button onClick={() => setShowCreateReservation(true)} className="room-btn" >Book Room</button>
-          )}
-          </div>
+    <div className="room-block">
+    <div className="images-field">
+    <div className="listed-images">
+    {/* <div className="brew-image-block" key={room.id}>
+    <button className="img-button"><img className="brew-image" src={room?.file_name}alt="" /></button>
+    </div> */}
+    </div>
+    <div className="center-image-container">
+        <img className="center-image" src={room?.file_name}alt="" />
+    </div>
+    </div>
+
+    <div className="brew-details-block">
+
+    <h3>{room?.location?.city}, {room?.location?.country}</h3>
+    {/* <div className="user-snippet">{users && <img className="profile-image" src={users[brew.user_id]?.image_url}
+    alt="" />} {users && users[brew.user_id]?.username}</div>
+    <p>{brew?.description}</p>
+    {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} */}
+
+    <p><b>${room?.price}</b></p>
+
+    {/* {brew.brew_tags.map(tag => {
+        <p>Tags: {tag?.name}</p>
+    })} */}
+    </div>
+
+    {/* <div>
+    {/* {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} */}
+    {/* <p><b>${brew?.price}</b></p> }
+    {brew?.for_sale ? <AddToCart brew={brew}/> : "No longer for Sale" }
+    {(brew?.for_sale && sessionUser?.id === brew?.user_id) && (
+    <button className="button purple" onClick={() => setShowEditForm(true)}>Show Edit Form</button>
+    )}
+    </div> */}
+
+{content}
+
+    </div>
+
+
+
+
+  //   <div>
+  //   <div className='room-block'>
+  //   <h1>{room?.location?.city}, {room?.location?.country}</h1>
+  //   <div className='info-image'>
+  //       <div className='room-info'>
+  //         <span>{avgRating} <i className="fa-solid fa-star fa-sm"></i></span>
+  //         <p>${room?.price}</p>
+  //         <div className='other-info'>
+  //         <p>{room?.total_bathrooms} Bathrooms </p>
+  //         <p>{room?.total_bedrooms} Bedrooms</p>
+  //         <p>Max Occupancy: {room?.total_occupancy}</p>
+  //         </div>
+  //     </div>
+  //       <img src={room?.file_name}
+  //       alt="" className='room-image'/>
+  //     <div>
+  //     </div>
+  //       <div>
+  //         <h4 className='description-tag'>{room?.description}</h4>
+  //       </div>
+  //     {(!showEditRoom && (sessionUser?.id === room?.owner_id)) && (
+  //           <button onClick={() => setShowEditRoom(true)} className="room-btn" >Edit</button>
+  //         )}
+  //         {(!showCreateReservation) && (
+  //           <button onClick={() => setShowCreateReservation(true)} className="room-btn" >Book Room</button>
+  //         )}
+  //         </div>
           
-  </div>
-   {content}
-  </div>
+  // </div>
+  //  {content}
+  // </div>
   
   )
 }
