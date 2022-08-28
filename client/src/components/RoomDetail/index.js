2401cc05d11ac436d7527294249cb31249c46568
Josh Keim <joshuamkeim@gmail.com>
2022-08-27 21:43:24 -0400

    making small changes

--- client/src/components/RoomDetail/index.js
+++ client/src/components/RoomDetail/index.js
@@ -26,6 +26,8 @@ function RoomDetail() {
  
 
     const room = useSelector(state => state.room?.room);
+    const userId = room?.owner_id
+    const reviews = room?.reviews
     let avgRating;
 
     if(room?.review.length > 0){
@@ -57,7 +59,7 @@ function RoomDetail() {
     }  else {
       content = (
         <div className='lower-block'>
-        < AssociatedBookings key={room?.id} room={room} user={sessionUser} />
+        {/* < AssociatedBookings key={room?.id} room={room} user={sessionUser} /> */}
         <ReviewPage key={room?.review.id} room={room} user={sessionUser} />
          </div>
       )
@@ -65,12 +67,50 @@ function RoomDetail() {
 
 
   return (
-    <div className="room-block">
+    <div className='room-page'>
+      <div className='room-header'>
+        <h2 className='title'>Extremely Nice House in Desert</h2>
+        <div className='room-rating-display'>
+          <p className="material-symbols-outlined">star</p>
+          {sessionUser?.id !== userId &&
+          <p className='booking__display-rating-text'>
+            {/* {rating ?
+              <p>{rating} &#8226;</p> :
+              <p onClick={() => setFirstReview(true)}
+                className='rating__first-text'
+              >
+                Be the first to leave a review
+              </p>
+            } */}
+          </p>
+          }
+          {sessionUser?.id === userId && !reviews?.length &&
+            <p className='booking__display-rating-text'>No reviews on your listing yet!</p>
+          }
+          <p className='booking__display-rating-amount'>
+            <p
+            onClick={() => console.log()}
+            className='rating__first-text'
+            >
+              {avgRating ? `${reviews?.length} ${reviews?.length > 1 ? 'reviews' : 'review'}` : null}
+            </p>
+          </p>
+          <p>&#8226;</p>
+          <h4 className='booking__display-header-location header-subtitle'>{room?.location?.city}, {room?.location?.country}</h4>
+        </div>
+      </div>
+      </div>
+
+
+
+
+
+   /* <div className="room-block">
     <div className="images-field">
     <div className="listed-images">
     {/* <div className="brew-image-block" key={room.id}>
     <button className="img-button"><img className="brew-image" src={room?.file_name}alt="" /></button>
-    </div> */}
+    </div> 
     </div>
     <div className="center-image-container">
         <img className="center-image" src={room?.file_name}alt="" />
@@ -83,28 +123,28 @@ function RoomDetail() {
     {/* <div className="user-snippet">{users && <img className="profile-image" src={users[brew.user_id]?.image_url}
     alt="" />} {users && users[brew.user_id]?.username}</div>
     <p>{brew?.description}</p>
-    {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} */}
+    {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} 
 
     <p><b>${room?.price}</b></p>
 
     {/* {brew.brew_tags.map(tag => {
         <p>Tags: {tag?.name}</p>
-    })} */}
+    })} 
     </div>
 
     {/* <div>
-    {/* {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} */}
-    {/* <p><b>${brew?.price}</b></p> }
+    {/* {ids.includes(brew.id) && <a href={brew?.pdf_url} download="true">Download</a>} */
+    /* <p><b>${brew?.price}</b></p> }
     {brew?.for_sale ? <AddToCart brew={brew}/> : "No longer for Sale" }
     {(brew?.for_sale && sessionUser?.id === brew?.user_id) && (
     <button className="button purple" onClick={() => setShowEditForm(true)}>Show Edit Form</button>
     )}
-    </div> */}
-
-{content}
+    </div> 
 
     </div>
+    {content}
 
+  */
 
 
 
